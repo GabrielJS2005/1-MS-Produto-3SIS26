950edeed136a1cb1640b924712549d22d088b29e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-11 22:32:18 -0300

    feat: completo aula 03

---
+++ src/main/java/com/github/GabrielJS2005/ms_produto/controller/ProdutoController.java
@@ -0,0 +1,35 @@
+package com.github.GabrielJS2005.ms_produto.controller;
+
+import com.github.GabrielJS2005.ms_produto.dto.ProdutoInputDTO;
+import com.github.GabrielJS2005.ms_produto.dto.ProdutoResponseDTO;
+import com.github.GabrielJS2005.ms_produto.entities.Produto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoResponseDTO>> getProduto() {
+
+        List<ProdutoResponseDTO> dto = ProdutoResponseDTO.createMock();
+
+        return (ResponseEntity.ok(dto));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoResponseDTO> createProduto(@RequestBody ProdutoInputDTO inputDTO) {
+
+        ProdutoResponseDTO dto = new ProdutoResponseDTO(1L, inputDTO.getNome(), inputDTO.getDescricao(),
+                inputDTO.getValor());
+
+        return (ResponseEntity.created(null).body(dto));
+
+    }
+
+}
